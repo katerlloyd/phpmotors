0011a6c6a622c537a224c2daf259e2a0cdb4df7c
katereclark <cla16022@byui.edu>
2022-02-21 16:29:42 -0700

    Added a 404 page

---
+++ views/404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page Not Found | PHP Motors</title>
+        <link rel="stylesheet" href="/phpmotors/css/styles.css" type="text/css" media="screen">
+    </head>
+    <body>
+        <header>
+           <?php require $_SERVER['DOCUMENT_ROOT'].'/phpmotors/snippets/header.php'; ?>
+        </header>
+        <nav>
+            <?php require $_SERVER['DOCUMENT_ROOT'].'/phpmotors/snippets/nav.php'; ?>
+        </nav>
+        <main>
+            <h1>Page Not Found</h1>
+            <p>Sorry, this page does not exist.
+        </main>
+        <footer>
+            <?php require $_SERVER['DOCUMENT_ROOT'].'/phpmotors/snippets/footer.php'; ?>
+        </footer>
+    </body>
+</html>
